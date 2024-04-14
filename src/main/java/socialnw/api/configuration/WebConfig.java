170ed6eb868d2a301431f64254bfde19a6644263
Do Chi Hieu <dochihieu1994@gmail.com>
2024-04-14 22:12:47 +0700

    commit

--- src/main/java/socialnw/api/configuration/WebConfig.java
+++ src/main/java/socialnw/api/configuration/WebConfig.java
@@ -1,53 +1,40 @@
 package socialnw.api.configuration;
 
-import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebConfig {
-	
-	private CustomUserDetailsService userDetailsService;
-	
+
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(request -> request.requestMatchers("/api/login", "/api/authenticate", "/api/register", "api-testing.html", "/swagger-ui/*").permitAll()
+		// @formatter:off
+		http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(request -> request.requestMatchers("/api/login", "/api/authenticate").permitAll()
+				.requestMatchers("/api/register").permitAll()
+				.requestMatchers("api-testing.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+				.requestMatchers("/error/**").permitAll()
 				.anyRequest().authenticated()).logout(logout -> logout.permitAll());
+		// @formater:on
 		return http.build();
 	}
 	
-//	@Bean
-//	public UserDetailsManager users(DataSource dataSource) {
-//		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//		UserDetails user = User.withUsername("email").password("password").passwordEncoder(encoder::encode).roles("USER").build();
-//		JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
-//		manager.createUser(user);
-//		return manager;
-//	}
-	
 	@Bean
-	public DaoAuthenticationProvider authenticationProvider() {
-		final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		authProvider.setUserDetailsService(userDetailsService);
-		authProvider.setPasswordEncoder(encoder());
-		return authProvider;
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
 	}
 	
 	@Bean
 	public PasswordEncoder encoder() {
+		// Default rounds: 10
 		return new BCryptPasswordEncoder();
 	}
 }
--- src/main/java/socialnw/api/controllers/AccountController.java
+++ src/main/java/socialnw/api/controllers/AccountController.java
@@ -1,7 +1,15 @@
 package socialnw.api.controllers;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +19,27 @@ import socialnw.api.entities.Account;
 @RequestMapping("/api")
 public class AccountController {
 	
+	@Autowired
+	private AuthenticationManager authManager;
+	
 	@PostMapping("register")
 	public ResponseEntity<Account> register() {
 		return null;
 	}
 
+	@PostMapping("login")
+	public ResponseEntity<Account> login(@RequestBody Map<String, String> params) {
+		String email = params.get("email");
+		String password = params.get("password");
+		Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		return ResponseEntity.ok(null);
+	}
+	
+	@PostMapping("authenticate")
+	public ResponseEntity<Account> authenticate(@RequestBody Map<String, String> params) {
+		String otp = params.get("otp");
+		String email = params.get("email");
+		return null;
+	}
 }
